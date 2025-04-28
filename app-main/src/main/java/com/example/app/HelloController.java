4a23a087203e068cc18d87056736f20b7ed835ef
Rob Blake <rob.bl8ke@outlook.com>
2025-04-28 10:41:15 +0200

    Add ClientProducer (Serialization problems through Kafka)

--- app-main/src/main/java/com/example/app/HelloController.java
+++ app-main/src/main/java/com/example/app/HelloController.java
@@ -1,6 +1,10 @@
 package com.example.app;
 
 import com.cyg.kafkacore.HelloWorldService;
+import com.example.app.kafka.ClientProducer;
+
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     private final HelloWorldService helloWorldService;
+    private final ClientProducer clientProducer;
 
-    public HelloController(HelloWorldService helloWorldService) {
+    public HelloController(HelloWorldService helloWorldService, ClientProducer clientProducer) {
         this.helloWorldService = helloWorldService;
+        this.clientProducer = clientProducer;
     }
 
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "world") String name) {
+        clientProducer.sendMessage(UUID.randomUUID(), UUID.randomUUID());
+        
         return helloWorldService.capitalize(name);
 
     }
---
+++ app-main/src/main/java/com/example/app/kafka/ClientProducer.java
@@ -0,0 +1,82 @@
+package com.example.app.kafka;
+
+import com.cyg.kafkacore.config.kafka.KafkaProducerConfig;
+import com.cyg.kafkacore.core.KafkaProducer;
+import com.example.app.dto.events.Header;
+import com.example.app.dto.events.SomethingHappened;
+import com.example.app.dto.events.SomethingHappenedBody;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ClientProducer extends KafkaProducer<SomethingHappened> {
+    @Value("${kafka.context-topic.name}")
+    private String topicName;
+
+    @Value("${kafka.producers.something-happened.event-type}")
+    private String eventType;
+
+    @Value("${kafka.producers.something-happened.schema-id}")
+    private String schemaId;
+
+    private String sourceId = "kafka-core-spike";
+
+    public ClientProducer(KafkaProducerConfig config, @Value("${kafka.context-topic.name}") String schemaName) {
+        super(config, schemaName);
+    }
+
+    @Override
+    public void prepareEventSpecificHeader(@NonNull SomethingHappened event) {
+        event.setHeader(Header.newBuilder(event.getHeader())
+                .setEventId(UUID.randomUUID().toString())
+                .setChecksumHash(UUID.randomUUID().toString())
+                .setRetryCount("2")
+                .setPriority("1")
+                .setSecurityContext("Nothing Here")
+                .setTenantId(UUID.randomUUID().toString())
+                .setCausationId(UUID.randomUUID().toString())
+                .setCorrelationId(UUID.randomUUID().toString())
+                .setContentType("application/avro")
+                .setEncoding("UTF-8")
+                .setDomain("Whatzit")
+                .setTimestamp(Instant.now())
+                .setEventType(eventType)
+                .setEventVersion("1")
+                .setAggregatedId(UUID.randomUUID().toString())
+                .setAggregatedType(UUID.randomUUID().toString())
+                .setSchemaId(schemaId)
+                .setSourceId(sourceId)
+                .setBusinessUnit("Domain")
+                .setMetadata(new HashMap<>())
+                .build());
+    }
+
+    public void sendMessage(UUID applicationId, UUID correlationId) {
+
+        SomethingHappenedBody body = SomethingHappenedBody.newBuilder()
+                .setApplicationId(applicationId.toString())
+                .build();
+
+        // Header header = Header.newBuilder()
+
+        //         .setEventType(eventType)
+        //         .setSchemaId(schemaId)
+        //         .setSourceId(sourceId)
+        //         .build();
+
+        // SomethingHappened event = new SomethingHappened(header, body);
+        // sendMessage(event, event.getBody().getApplicationId(), topicName);
+        SomethingHappened event = new SomethingHappened();
+        event.setBody(body);
+        sendMessage(event, UUID.randomUUID().toString(), topicName);
+    }
+}
